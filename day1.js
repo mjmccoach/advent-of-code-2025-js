038a0732a2e2a31b6c6fe01d0ec9c3cae911cab7
Matthew <m.mccoach88@gmail.com>
2025-12-05 14:34:29 +0000

    Solve in native js.

---
+++ day1.js
@@ -0,0 +1,35 @@
+"use strict"
+const fs = require("fs");
+const input = fs.readFileSync("day01-input.txt", "utf8").trim()
+
+function main() {
+
+    let pointed = 50
+    
+    let counter = 0
+
+    const rawTokens = input.split("\n")
+
+    for (const rawToken of rawTokens) { 
+    
+        const token = rawToken.trim()
+        
+        const sign = (token[0] == "L") ? -1 : +1
+        
+        const originalNumber = parseInt(token.substr(1))
+        
+        const number = originalNumber % 100 // because some numbers are greater than 100
+        
+        pointed += (sign * number)
+        
+        if (pointed < 0) { pointed += 100 } else if (pointed > 99) { pointed -= 100 }
+        
+        if (pointed == 0) { counter += 1 }
+    }
+    
+    console.log("the answer is", counter)
+}
+
+console.time("execution time")
+main()
+console.timeEnd("execution time") // 2ms
